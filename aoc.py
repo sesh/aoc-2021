49ccd4b5e9c936722122e43aa094a81b0cc5ebb0
Brenton Cleeland <brenton@brntn.me>
2021-12-03 17:31:21 +1100

    Day #3. Very loopy.

--- aoc.py
+++ aoc.py
@@ -1,5 +1,4 @@
-
-def d1(depths: list[str], window: int=3) -> int:
+def d1(depths: list[str], window: int = 3) -> int:
     count = 0
     for x in range(1, len(depths) - window + 1):
         count += sum(depths[x : x + window]) > sum(depths[x - 1 : x - 1 + window])
@@ -38,6 +37,51 @@ def d2_with_aim(course: list[str]) -> int:
     return depth * distance
 
 
+def d3_frequencies(readings):
+    frequencies = [0 for _ in readings[0]]
+
+    for reading in readings:
+        reading = reading.strip()
+        for i, x in enumerate(reading):
+            if x == "1":
+                frequencies[i] += 1
+            else:
+                frequencies[i] -= 1
+
+    return frequencies
+
+
+def d3_get_reading_by_bit_criteria(readings, bit_criteria):
+    for i in range(len(readings[0])):
+        frequencies = d3_frequencies(readings)
+        f = frequencies[i]
+
+        bit = bit_criteria(f)
+        readings = [x for x in readings if x[i] == bit]
+
+        if len(readings) == 1:
+            return int(readings[0], 2)
+
+
+def d3(readings):
+    frequencies = d3_frequencies(readings)
+
+    gamma = int("".join(["1" if x >= 0 else "0" for x in frequencies]), 2)
+    epsilon = int("".join(["0" if x >= 0 else "1" for x in frequencies]), 2)
+
+    return gamma * epsilon
+
+
+def d3_life_support(readings):
+    o2_generator = d3_get_reading_by_bit_criteria(
+        readings, lambda f: "1" if f >= 0 else "0"
+    )
+    co2_scrubber = d3_get_reading_by_bit_criteria(
+        readings, lambda f: "1" if f < 0 else "0"
+    )
+    return o2_generator * co2_scrubber
+
+
 if __name__ == "__main__":
     d1_1 = d1([int(x) for x in open("inputs/d1.txt").readlines() if x], 1)
     print("1.1:", d1_1)  # 1553
@@ -54,3 +98,11 @@ if __name__ == "__main__":
     d2_2 = d2_with_aim(open("inputs/d2.txt").readlines())
     print("2.2:", d2_2)  # 1727785422
     assert d2_2 == 1727785422
+
+    d3_1 = d3([x.strip() for x in open("inputs/d3.txt").readlines()])
+    print("3.1:", d3_1)  # 1307354
+    assert d3_1 == 1307354
+
+    d3_2 = d3_life_support([x.strip() for x in open("inputs/d3.txt").readlines()])
+    print("3.2:", d3_2)  # 1307354
+    assert d3_2 == 482500
--- pre.sh
+++ pre.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
 black . --target-version py310
+python3 aoc.py
