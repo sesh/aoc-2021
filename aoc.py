d126dc07a7644231c37ae47cfc1414f21504c696
Brenton Cleeland <brenton@brntn.me>
2021-12-04 20:50:46 +1100

    Day #4

--- aoc.py
+++ aoc.py
@@ -82,6 +82,83 @@ def d3_life_support(readings):
     return o2_generator * co2_scrubber
 
 
+def d4_load_boards(input):
+    boards = []
+    board = []
+
+    for l in input[2:]:
+        if not l:
+            boards.append(board)
+            board = []
+        else:
+            board.append([x.strip() for x in l.split()])
+    boards.append(board)
+    return boards
+
+
+def d4_check_if_winner(board):
+    for row in board:
+        if len([x for x in row if x.endswith('*')]) == len(row):
+            return True
+    
+    for i in range(len(board[0])):
+        if len([row[i] for row in board if row[i].endswith("*")]) == len(board[0]):
+            return True
+
+    return False
+
+
+def d4_mark_draw_on_board(draw, board):
+    for row in board:
+        for i, n in enumerate(row):
+            if n == draw:
+                row[i] = n + "*"
+
+
+def d4_tally_board(board):
+    total = 0
+    for row in board:
+        for x in row:
+            if not x.endswith("*"):
+                total += int(x)
+    return total
+
+
+def d4_bingo(input):
+    draws = input[0].split(',')
+    boards = d4_load_boards(input)
+
+    for draw in draws:
+        for board in boards:
+            d4_mark_draw_on_board(draw, board)
+            winner = d4_check_if_winner(board)
+
+            if winner:
+                total = d4_tally_board(board)
+                return total * int(draw)
+
+
+def d4_bingo_last(input):
+    draws = input[0].split(',')
+    boards = d4_load_boards(input)
+
+    completed_boards = []
+    for draw in draws:
+        for board_id, board in enumerate(boards):
+            if board_id in completed_boards:
+                continue
+
+            d4_mark_draw_on_board(draw, board)
+            winner = d4_check_if_winner(board)
+
+            if winner:
+                completed_boards.append(board_id)
+
+                if len(completed_boards) == len(boards):
+                    total = d4_tally_board(board)
+                    return total * int(draw)
+
+
 if __name__ == "__main__":
     d1_1 = d1([int(x) for x in open("inputs/d1.txt").readlines() if x], 1)
     print("1.1:", d1_1)  # 1553
@@ -106,3 +183,11 @@ if __name__ == "__main__":
     d3_2 = d3_life_support([x.strip() for x in open("inputs/d3.txt").readlines()])
     print("3.2:", d3_2)  # 1307354
     assert d3_2 == 482500
+
+    d4_1 = d4_bingo([x.strip() for x in open("inputs/d4.txt").readlines()])
+    print("4.1:", d4_1)
+    assert d4_1 == 39984
+
+    d4_2 = d4_bingo_last([x.strip() for x in open("inputs/d4.txt").readlines()])
+    print("4.2:", d4_2)
+    assert d4_2 == 8468
